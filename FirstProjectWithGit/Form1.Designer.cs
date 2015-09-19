9d4b24ac8a269a7b75696c79e5fcac2453a6c08f
AleksandraDolganovskaya <adolganovskaya@mail.ru>
2015-09-19 23:10:23 +0300

    Добавление проекта КАЛЬКУЛЯТОР в репозиторий. Реализован метод СЛОЖЕНИЯ

---
+++ FirstProjectWithGit/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace FirstProjectWithGit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxForFirstNumber = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtBoxForSecondNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxResult = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtBoxForFirstNumber
+            // 
+            this.txtBoxForFirstNumber.Location = new System.Drawing.Point(12, 12);
+            this.txtBoxForFirstNumber.Name = "txtBoxForFirstNumber";
+            this.txtBoxForFirstNumber.Size = new System.Drawing.Size(211, 20);
+            this.txtBoxForFirstNumber.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Location = new System.Drawing.Point(12, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(211, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Операции";
+            // 
+            // txtBoxForSecondNumber
+            // 
+            this.txtBoxForSecondNumber.Location = new System.Drawing.Point(12, 144);
+            this.txtBoxForSecondNumber.Name = "txtBoxForSecondNumber";
+            this.txtBoxForSecondNumber.Size = new System.Drawing.Size(211, 20);
+            this.txtBoxForSecondNumber.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(100, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "=";
+            // 
+            // txtBoxResult
+            // 
+            this.txtBoxResult.Location = new System.Drawing.Point(12, 201);
+            this.txtBoxResult.Name = "txtBoxResult";
+            this.txtBoxResult.Size = new System.Drawing.Size(211, 20);
+            this.txtBoxResult.TabIndex = 4;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnAdd.Location = new System.Drawing.Point(47, 19);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(33, 28);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClear.Location = new System.Drawing.Point(8, 19);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(33, 28);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "C";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(235, 232);
+            this.Controls.Add(this.txtBoxResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxForSecondNumber);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtBoxForFirstNumber);
+            this.Name = "Form1";
+            this.Text = "Калькулятор";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxForFirstNumber;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtBoxForSecondNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxResult;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ FirstProjectWithGit/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FirstProjectWithGit
+{
+    public partial class Form1 : Form
+    {
+        Operation operation = new Operation();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtBoxResult.Text = operation.Add(Convert.ToDouble(txtBoxForFirstNumber.Text), Convert.ToDouble(txtBoxForSecondNumber.Text)).ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Неправильный ввод!");
+                CLear();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            CLear();
+        }
+
+        private void CLear()
+        {
+            txtBoxForFirstNumber.Clear();
+            txtBoxForSecondNumber.Clear();
+            txtBoxResult.Clear();
+        }
+    }
+}
